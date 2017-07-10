f23a914637605eb40819128b1120976266453ffb
BeataSkuczynska <b.skuczynska@student.uw.edu.pl>
2017-07-10 18:21:35 +0200

    Not working yet SearchPanel, but storing already all components.

---
+++ src/main/java/pl/nowy/GraphicElements/SearchPanel.java
@@ -0,0 +1,44 @@
+package pl.nowy.GraphicElements;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+
+
+/**
+ * Created by Wiedźma on 2017-07-10.
+ */
+public class SearchPanel extends HorizontalLayout {
+
+   // private Grid<Entry> grid;
+    private TextField searchField;
+    private Button searchButton;
+
+    public TextField getSearchField() { return searchField; }
+
+    public void setSearchField(TextField searchField) { this.searchField = searchField; }
+
+    public Button getSearchButton() { return searchButton; }
+
+    public void setSearchButton(Button searchButton) { this.searchButton = searchButton; }
+
+    public SearchPanel() {
+        init();
+    }
+
+    public void init(){
+
+        TextField searchField = new TextField();
+        Button searchButton = new Button("Szukaj");
+
+        addStyleName("search-package");
+        searchField.addStyleName("search-field");
+        searchButton.addStyleName("search-button");
+
+        searchField.setValue("Wpisz czasownik, którego szukasz");
+
+        addComponent(searchField);
+        addComponent(searchField);
+        addComponent(searchButton);
+    }
+}
