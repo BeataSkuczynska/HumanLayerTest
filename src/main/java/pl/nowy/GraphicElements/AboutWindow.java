bd6f8b20fecb05dc79a81d6dc45db27d27922708
Kasia <kasi.skowronska@gmail.com>
2017-08-14 21:27:29 +0200

    new branch

---
+++ src/main/java/pl/nowy/GraphicElements/AboutWindow.java
@@ -0,0 +1,46 @@
+package pl.nowy.GraphicElements;
+
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.*;
+
+/**
+ * Created by Kasia on 12.07.2017.
+ */
+public class AboutWindow extends VerticalLayout {
+
+    private TextArea aboutInformation;
+    private HorizontalLayout sample;
+    private TopPanel top;
+
+    public AboutWindow() {
+
+        setMargin(new MarginInfo(false, false, false, false));
+
+        top = new TopPanel();
+        addComponent(top);
+        top.setSizeFull();
+
+        setSpacing(false);
+        setMargin(false);
+        setSizeFull();
+
+        aboutInformation = new TextArea();
+        aboutInformation.setValue("Human Layer");
+        //addComponent(aboutInformation);
+
+        sample = new HorizontalLayout();
+        sample.addStyleName("outlined");
+        sample.setSpacing(false);
+        sample.setMargin(false);
+        sample.setSizeFull();
+
+        for (int i = 0; i < 3; i++) {
+            final Button childComponent = new Button("kkk");
+            childComponent.setSizeFull();
+            sample.addComponent(childComponent);
+        }
+
+        addComponent(sample);
+
+    }
+}
